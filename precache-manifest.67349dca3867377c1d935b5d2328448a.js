133ec092cb5973984c3fb71fbfaa8eba02a42a68
unknown <thetradecoder@gmail.com>
2020-03-24 10:44:29 +0600

    Updates

---
+++ precache-manifest.67349dca3867377c1d935b5d2328448a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45bd45427b29eb8bd7563176fc13c59",
+    "url": "/css-color-picker/index.html"
+  },
+  {
+    "revision": "9d55a5ae0c374dc10c45",
+    "url": "/css-color-picker/static/css/main.4a4b006d.chunk.css"
+  },
+  {
+    "revision": "c5708a32470d92551a04",
+    "url": "/css-color-picker/static/js/2.816c0a17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-color-picker/static/js/2.816c0a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d55a5ae0c374dc10c45",
+    "url": "/css-color-picker/static/js/main.bc1bb074.chunk.js"
+  },
+  {
+    "revision": "aa59c5d974e1f8456a85",
+    "url": "/css-color-picker/static/js/runtime-main.485b76b7.js"
+  }
+]);
