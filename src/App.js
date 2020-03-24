ae734fbc828d5064cb08ac313c8b287680f15284
unknown <thetradecoder@gmail.com>
2020-03-24 10:49:24 +0600

    css color picker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class App extends React.Component {
+
+  componentDidMount(){
+    let colorInput = document.querySelector('#color'); 
+let colorName = document.querySelector('#color-name');
+colorInput.addEventListener('input', ()=>{
+  let color = colorInput.value;
+  colorName.value = color;
+  document.body.style.backgroundColor=color;
+}); 
+
+let hColorInput = document.querySelector('#h-color');
+let hColorName= document.querySelector('#h-color-name');
+hColorInput.addEventListener('input', ()=>{
+  let color = hColorInput.value;
+  hColorName.value = color;
+  document.querySelector('#body-content h1').style.color=color;
+});
+
+let tColorInput = document.querySelector('#t-color');
+let tColorName = document.querySelector('#t-color-name');
+
+tColorInput.addEventListener('input', ()=>{
+  let color = tColorInput.value;
+  document.querySelector('#body-content p').style.color=color;
+  tColorName.value=color;
+})
+
+  };
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <section id='sidebar-menu'>
+  <div className='sidebar-menu'>
+    <h1 className='text-center'>CSS Color Picker</h1>
+    <div>
+      <h3>Change Background</h3>
+      <div className='bg-color'>
+        <input type='color' id='color'/>
+        <input type='text' id='color-name' />
+      </div>
     </div>
+    
+    <div>
+      <h3>Change Header color</h3>
+      <div className='h-color'>
+        <input type='color' id='h-color'/>
+        <input type='text' id='h-color-name' />
+      </div>
+    </div>
+    <div>
+      <h3>Change Text color</h3>
+      <div className='t-color'>
+        <input type='color' id='t-color'/>
+        <input type='text' id='t-color-name' />
+      </div>
+    </div>
+  <div>
+    <footer>
+      Dev:<br/>
+      Mamun Abdullah <br/>
+      THE TRADE CODER
+
+    </footer>
+    </div>  
+  </div>
+  
+</section>
+
+<section className='body-content' id='body-content'>
+  <h1> Header- Change color from the menu with color picker </h1>
+  <p>Text- Change color from the menu with color picker</p>
+  
+</section>
+</div>
+
   );
+  }
 }
 
-export default App;
+
